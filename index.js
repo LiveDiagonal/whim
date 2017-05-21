456d348c4debcd92ebe17806a1c026cc557d7f7d
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-20 23:47:33 -0500

    Improve processResult

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 require('dotenv').config()
 
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var bodyParser = require('body-parser');
-var request = require('request');
-var twilio = require('twilio');
-var MessagingResponse = twilio.twiml.MessagingResponse;
+var bodyParser = require('body-parser')
+var request = require('request')
+var twilio = require('twilio')
+var MessagingResponse = twilio.twiml.MessagingResponse
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
 app.get('/', function (request, response) {
   response.render("pages/index")
-});
+})
 
 app.post('/command', function(request, response) {
   var processResult = function (result) {
     response.json(result);
-  };
+  }
 
   processCommand(request.body.command, processResult);
-});
+})
 
 app.post('/whim', function(req, res, next) {
   var twiml = new MessagingResponse();
@@ -37,35 +37,49 @@ app.post('/whim', function(req, res, next) {
   }
 
   processCommand(req.body.Body, processResult);  
-});
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-});
+})
 
 // Workspace
 
-var commands = {
-  "mlb": "http://whim.mlb.livediagonal.com/game"
-}
 
-var processCommand = function (command, processResult) {
-  var app = command.split(" ")[0];
-  var url = commands[app];
+var processCommand = function (commandStr, processResult) {
+  var commandObj = parseCommand(commandStr)
+  var appUrl = getAppUrl(commandObj.app)
 
-  if (url) {
+  if (appUrl) {
     request.get({
-      url: url,
+      url: appUrl,
+      json: true,
       qs: {
-        team: command.split(" ")[1]
+        command: commandObj.parameters
       }
     }, function (error, response, body) {
-      processResult(JSON.parse(body));
-    });
+      processResult(body);
+    })
   } else {
     processResult({
-      success: false,
-      message: "Failure: app " + app + " not found."
-    });
+      message: "Failure: app " + commandObj.app + " not found."
+    })
+  }
+}
+
+
+var getAppUrl = function(name) {
+  return {
+    "mlb": "http://whim.mlb.livediagonal.com/game"
+  }[name]
+}
+
+
+var parseCommand = function (command) {
+  var matches = command.toLowerCase().match(/(\w+)\s*(.*)/)
+  
+  return {
+    app: (matches.length > 1) ? matches[1] : null,
+    parameters: (matches.length > 2) ? matches[2] : null,
   }
 }
