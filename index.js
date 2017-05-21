e97e5318e3608a987c1a2be87ed056babeddb8de
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-20 21:21:09 -0500

    Add dotenv

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var twilio = require('twilio');
 var twilioClient = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
+require('dotenv').config()
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
