dac58087e487ac7bd97b4b33cf2683f2b7ddb23d
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-20 22:33:39 -0500

    Working mlb poc

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var request = require('request');
 var twilio = require('twilio');
 var twilioClient = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
@@ -19,11 +20,12 @@ app.get('/', function (request, response) {
 	response.render("pages/index")
 });
 
-app.get('/command', function(request, response) {
-  response.send({
-   	success: true,
-	message: "hello world"
-  });
+app.post('/command', function(request, response) {
+  var process_result = function (result) {
+    response.json(result);
+  };
+
+  process_command(request.body.command, process_result);
 });
 
 app.post('/whim', function(req, res, next) {
@@ -42,3 +44,31 @@ app.post('/whim', function(req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+// Workspace
+
+var commands = {
+  "mlb": "http://whim.mlb.livediagonal.com/game"
+}
+
+var process_command = function (command, process_result) {
+  var app = command.split(" ")[0];
+  var url = commands[app];
+
+  if (url) {
+    request.get({
+      url: url,
+      qs: {
+        team: command.split(" ")[1]
+      }
+    }, function (error, response, body) {
+      console.log(body);
+      process_result(body);
+    });
+  } else {
+    process_result({
+      success: false,
+      message: "Failure: app " + app + " not found."
+    });
+  }
+}
