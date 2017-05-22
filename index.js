4d920081e4af7e912a8900795e7fe869bb7947cf
Anthony D'Addeo <amdaddeo@gmail.com>
2017-05-21 23:01:11 -0500

    Refactor processCommand into own file, use Promises

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var app = express()
 
 
 var bodyParser = require('body-parser')
-var request = require('request')
 var twilio = require('twilio')
 var MessagingResponse = twilio.twiml.MessagingResponse
+
+var processCommand = require('./lib/process_command')
+
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -26,65 +28,24 @@ app.get('/', function (request, response) {
 })
 
 app.post('/command', function(request, response) {
-  var processResult = function (result) {
-    response.json(result);
-  }
-
-  processCommand(request.body.command, processResult);
+  processCommand(request.body.command)
+    .then(function(message) {
+      response.send(message)
+    })
 })
 
 app.post('/whim', function(req, res, next) {
   var twiml = new MessagingResponse();
-  var processResult = function(result) {
-    // hack for annoying trial message
-    twiml.message("-\n" + result.message)
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
-  }
 
-  processCommand(req.body.Body, processResult)
+  processCommand(req.body.Body)
+    .then(function(message) {
+      // hack for annoying trial message
+      twiml.message("-\n" + message)
+      res.writeHead(200, {'Content-Type': 'text/xml'})
+      res.end(twiml.toString())
+    })
 })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'))
 })
-
-// Workspace
-
-var processCommand = function (commandStr, processResult) {
-  var commandObj = parseCommand(commandStr)
-  var appUrl = getAppUrl(commandObj.app)
-
-  if (appUrl) {
-    request.get({
-      url: appUrl,
-      json: true,
-      qs: {
-        command: commandObj.parameters
-      }
-    }, function (error, response, body) {
-      processResult(body);
-    })
-  } else {
-    processResult({
-      message: "Failure: app " + commandObj.app + " not found."
-    })
-  }
-}
-
-
-var getAppUrl = function(name) {
-  return {
-    "mlb": "http://whim.mlb.livediagonal.com/execute"
-  }[name]
-}
-
-
-var parseCommand = function (command) {
-  var matches = command.toLowerCase().match(/(\w+)\s*(.*)/)
-  
-  return {
-    app: (matches.length > 1) ? matches[1] : null,
-    parameters: (matches.length > 2) ? matches[2] : null,
-  }
-}
